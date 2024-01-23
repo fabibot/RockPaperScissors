d95a6f0227dcab6f1b37723946d0b8084da09f3b
fabibot <fabulousfab.bellon@gmail.com>
2024-01-23 13:18:29 +0100

    finished rock-paper-scissors excercies

---
+++ main.js
@@ -0,0 +1,106 @@
+const rockButton = document.querySelector("#rockButton");
+const paperButton = document.querySelector("#paperButton");
+const scissorsButton = document.querySelector("#scissorsButton");
+let scoreTracker = [0, 0];
+
+let round = 0;
+const roundConter = document.querySelector('#roundConter');
+
+let playerChoice;
+rockButton.addEventListener("click", () => {
+    playerChoice = "rock";
+    scoreTracker =  gestionScore(scoreTracker, playRound(playerChoice));
+    roundConter.textContent = "Round " + ++round;
+
+});
+
+paperButton.addEventListener("click", () => {
+    playerChoice = "paper";
+    scoreTracker =  gestionScore(scoreTracker, playRound(playerChoice));
+    roundConter.textContent = "Round " + ++round;
+
+});
+
+scissorsButton.addEventListener("click", () => {
+    playerChoice = "scissors";
+    scoreTracker =  gestionScore(scoreTracker, playRound(playerChoice));
+    roundConter.textContent = "Round " + ++round;
+
+});
+    
+
+const DOMcomputerChoice = document.querySelector('#computerChoice');
+let choicesList = ["rock", "paper", "scissors" ];
+function getComputerChoice() {
+    let indexChoice = Math.floor(Math.random() * choicesList.length);
+    let choice = choicesList[indexChoice];
+    DOMcomputerChoice.textContent = choice;
+    return choice;
+};
+
+
+
+const DOMroundResult = document.querySelector("#roundResult");
+function playRound (playerSelection){
+    let computerSelection = getComputerChoice();
+    let roundResult;
+    let addScoreTracker = [];
+    if( computerSelection == playerSelection){
+        roundResult = "Egality ! Play again ";
+        addScoreTracker = [ 0, 0 ];
+    }
+    if (playerSelection == "scissors" && computerSelection == "paper"){
+        roundResult = "You win ! Scissors beats paper."
+        addScoreTracker = [1 , 0];
+    }
+    if (playerSelection == "paper" && computerSelection == "rock"){
+        roundResult = "You win ! Paper beats rock.";
+        addScoreTracker = [1, 0];
+    }
+    if (playerSelection == "rock" && computerSelection == "scissors"){
+        roundResult = "You win ! Rock beats scissors."
+        addScoreTracker = [1, 0];
+    }
+    if (computerSelection == "scissors" && playerSelection == "paper"){
+        roundResult = "You loose... Scissors beats paper."
+        addScoreTracker = [0, 1];
+    }
+    if (computerSelection == "paper" && playerSelection == "rock"){
+        roundResult = "You loose... Paper beats rock."
+        addScoreTracker = [0, 1];
+    }
+    if (computerSelection == "rock" && playerSelection == "scissors"){
+        roundResult = "You loose... Rock beats scissors."
+        addScoreTracker = [0, 1];
+    }
+    DOMroundResult.textContent = roundResult;
+    console.log(roundResult);
+
+    return addScoreTracker;
+}
+
+
+const DOMscoreTracker = document.querySelector('#scoreTracker');
+const backcolor = document.querySelector('body');
+function gestionScore (currentScore, scoreToAdd){
+    let newScore = currentScore;
+    console.log(scoreToAdd);
+    newScore[0] = currentScore[0] + scoreToAdd[0];
+    newScore[1] = currentScore[1] + scoreToAdd[1];
+    console.log(newScore);
+    DOMscoreTracker.textContent = "SCORE =>>>> Player : " + newScore[0] + " Computer : " + newScore[1];
+    if(newScore[0] == 5){
+        backcolor.style.backgroundColor = "green";
+        backcolor.textContent = "YOU WIN !!!!";
+        backcolor.style.fontSize = "200px";
+        console.log("end of the game");
+    } else if(newScore[1] == 5) {
+        backcolor.style.backgroundColor = "red";
+        backcolor.textContent = "Looser...";
+        backcolor.style.fontSize = "200px";
+    }
+    return newScore;
+}
+
+
+
